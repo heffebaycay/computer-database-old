7483001807f3ab48e8983fa60ba83d636702dabc
Heffebaycay <fchebel@gmail.com>
2013-09-10 21:10:27 +0200

    Revert "Init eclipse project. Added gitignore. Ready to use"
    This reverts commit ae8ecb50802347fc09cfe587c463e9a280420e1b.

--- sql/2.sql
+++ sql/2.sql
@@ -1,3 +1,6 @@
+# --- Sample dataset
+
+# --- !Ups
 
 insert into company (id,name) values (  1,'Apple Inc.');
 insert into company (id,name) values (  2,'Thinking Machines');
@@ -617,3 +620,7 @@ insert into computer (id,name,introduced,discontinued,company_id) values (572,'D
 insert into computer (id,name,introduced,discontinued,company_id) values (573,'Gateway LT3103U','2008-01-01',null,null);
 insert into computer (id,name,introduced,discontinued,company_id) values (574,'iPhone 4S','2011-10-14',null,1);
 
+# --- !Downs
+
+delete from computer;
+delete from company;
