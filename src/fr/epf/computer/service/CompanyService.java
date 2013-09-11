407bcc2021a6ab1c53bd7982751cc6cbbbfca25e
Heffebaycay <fchebel@gmail.com>
2013-09-11 22:50:18 +0200

    Moar javadoc

--- src/fr/epf/computer/service/CompanyService.java
+++ src/fr/epf/computer/service/CompanyService.java
@@ -6,8 +6,25 @@ import java.util.List;
 
 public interface CompanyService {
 
+    /**
+     * @see fr.epf.computer.dao.CompanyDao#getCompanies()
+     */
     List<Company> getCompanies();
+
+
+    /**
+     *
+     * @see fr.epf.computer.dao.CompanyDao#searchByName(String)
+     */
     List<Company> searchByName(String name);
+
+    /**
+     * @see fr.epf.computer.dao.CompanyDao#create(fr.epf.computer.domain.Company)
+     */
     void create(Company company);
+
+    /**
+     * @see fr.epf.computer.dao.CompanyDao#findById(long)
+     */
     Company findById(long id);
 }
